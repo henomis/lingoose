ca912362226a4f2aa91d045b3821568079fbabf0
Simone Vellei <henomis@gmail.com>
2023-05-13 17:47:33 +0200

    feat: add csv loader (#65)

---
+++ examples/loader/csv/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/henomis/lingoose/loader"
+)
+
+func main() {
+
+	l := loader.NewCSVLoader("/tmp/cities.csv").WithLazyQuotes()
+
+	docs, err := l.Load(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(docs[0].Content)
+
+}
