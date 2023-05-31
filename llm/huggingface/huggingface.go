dd0588c4dba8fbb60c2d3a8bae2461c001088b0f
Simone Vellei <henomis@gmail.com>
2023-05-31 10:07:07 +0200

    fix

--- llm/huggingface/huggingface.go
+++ llm/huggingface/huggingface.go
@@ -1,13 +1,8 @@
 package huggingface
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 )
 
@@ -33,12 +28,6 @@ type huggingFace struct {
 	topK        *int
 	topP        *float32
 	verbose     bool
-}
-
-type Options struct {
-	UseGPU       *bool `json:"use_gpu,omitempty"`
-	UseCache     *bool `json:"use_cache,omitempty"`
-	WaitForModel *bool `json:"wait_for_model,omitempty"`
 }
 
 func New(model string, temperature float32, verbose bool) *huggingFace {
@@ -112,78 +101,4 @@ func (h *huggingFace) Completion(ctx context.Context, prompt string) (string, er
 	}
 
 	return output, nil
-}
-
-func (h *huggingFace) doRequest(ctx context.Context, jsonBody []byte, model string) ([]byte, error) {
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, APIBaseURL+model, bytes.NewBuffer(jsonBody))
-	if err != nil {
-		return nil, err
-	}
-	if req == nil {
-		return nil, errors.New("nil request created")
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+h.token)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	err = checkRespForError(respBody)
-	if err != nil {
-		return nil, err
-	}
-
-	return respBody, nil
-}
-
-type apiError struct {
-	Error string `json:"error,omitempty"`
-}
-
-type apiErrors struct {
-	Errors []string `json:"error,omitempty"`
-}
-
-func checkRespForError(respJSON []byte) error {
-	{
-		buf := make([]byte, len(respJSON))
-		copy(buf, respJSON)
-		apiErr := apiError{}
-		err := json.Unmarshal(buf, &apiErr)
-		if err != nil {
-			return err
-		}
-		if apiErr.Error != "" {
-			return errors.New(string(respJSON))
-		}
-	}
-
-	{
-		buf := make([]byte, len(respJSON))
-		copy(buf, respJSON)
-		apiErrs := apiErrors{}
-		err := json.Unmarshal(buf, &apiErrs)
-		if err != nil {
-			return err
-		}
-		if apiErrs.Errors != nil {
-			return errors.New(string(respJSON))
-		}
-	}
-
-	return nil
-}
-
-func debugCompletion(prompt string, content string) {
-	fmt.Printf("---USER---\n%s\n", prompt)
-	fmt.Printf("---AI---\n%s\n", content)
 }
---
+++ llm/huggingface/shared.go
@@ -0,0 +1,14 @@
+package huggingface
+
+import "fmt"
+
+type Options struct {
+	UseGPU       *bool `json:"use_gpu,omitempty"`
+	UseCache     *bool `json:"use_cache,omitempty"`
+	WaitForModel *bool `json:"wait_for_model,omitempty"`
+}
+
+func debugCompletion(prompt string, content string) {
+	fmt.Printf("---USER---\n%s\n", prompt)
+	fmt.Printf("---AI---\n%s\n", content)
+}
