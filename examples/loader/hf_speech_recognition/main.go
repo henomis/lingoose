891315d70306e8c8b39156d193dd9ea12a5f7b99
Simone Vellei <henomis@gmail.com>
2023-06-02 00:00:25 +0200

    feat: add HF speech recognition loader (#78)

---
+++ examples/loader/hf_speech_recognition/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/henomis/lingoose/loader"
+)
+
+func main() {
+
+	l := loader.NewHFSpeechRecognitionLoader("/tmp/hello.mp3")
+
+	docs, err := l.Load(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(docs[0].Content)
+
+}
