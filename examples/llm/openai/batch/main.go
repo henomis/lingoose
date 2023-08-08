e001ec48701f969cbc16de057da92c13ef2f12ac
Simone Vellei <henomis@gmail.com>
2023-08-08 12:34:25 +0200

    chore: add openai batch completion

---
+++ examples/llm/openai/batch/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/henomis/lingoose/llm/openai"
+)
+
+func main() {
+
+	llm := openai.NewCompletion()
+
+	outputs, err := llm.BatchCompletion(
+		context.Background(),
+		[]string{
+			"Tell me a joke about geese",
+			"What is the NATO purpose?",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%#v\n", outputs)
+
+	var output1, output2 string
+
+	err = llm.BatchCompletionStream(
+		context.Background(),
+		[]openai.OpenAIStreamCallback{
+			func(output string) {
+				fmt.Printf("{%s}", output)
+				output1 += output
+			},
+			func(output string) {
+				fmt.Printf("[%s]", output)
+				output2 += output
+			},
+		},
+		[]string{
+			"Tell me a joke about geese",
+			"What is the NATO purpose?",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("------")
+	fmt.Printf("%s\n", output1)
+	fmt.Printf("%s\n", output2)
+
+}
