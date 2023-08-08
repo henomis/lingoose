af7112493be1310bd55f2d6871a4ff976b67f9ee
Simone Vellei <henomis@gmail.com>
2023-08-08 16:07:57 +0200

    Feat Add Cohere Rerank

---
+++ transformer/cohere-rerank.go
@@ -0,0 +1,109 @@
+package transformer
+
+import (
+	"context"
+	"os"
+
+	coherego "github.com/henomis/cohere-go"
+	"github.com/henomis/cohere-go/model"
+	"github.com/henomis/cohere-go/request"
+	"github.com/henomis/cohere-go/response"
+	"github.com/henomis/lingoose/document"
+	"github.com/henomis/lingoose/types"
+)
+
+const (
+	defaultCohereRerankMaxChunksPerDoc = 10
+	defaultCohereRerankTopN            = -1
+	CohereRerankScoreMetdataKey        = "cohere-rerank-score"
+)
+
+type CohereRerank struct {
+	client          *coherego.Client
+	maxChunksPerDoc int
+	topN            int
+}
+
+func NewCohereRerank() *CohereRerank {
+	return &CohereRerank{
+		client:          coherego.New(os.Getenv("COHERE_API_KEY")),
+		maxChunksPerDoc: defaultCohereRerankMaxChunksPerDoc,
+		topN:            defaultCohereRerankTopN,
+	}
+}
+
+func (c *CohereRerank) WithMaxChunksPerDoc(maxChunksPerDoc int) *CohereRerank {
+	c.maxChunksPerDoc = maxChunksPerDoc
+	return c
+}
+
+func (c *CohereRerank) WithAPIKey(apiKey string) *CohereRerank {
+	c.client = coherego.New(apiKey)
+	return c
+}
+
+func (c *CohereRerank) WithTopN(topN int) *CohereRerank {
+	c.topN = topN
+	return c
+}
+
+func (c *CohereRerank) Rerank(ctx context.Context, query string, documents []document.Document) ([]document.Document, error) {
+
+	if c.topN == defaultCohereRerankTopN {
+		c.topN = len(documents)
+	}
+
+	resp := &response.Rerank{}
+	err := c.client.Rerank(
+		context.Background(),
+		&request.Rerank{
+			ReturnDocuments: false,
+			MaxChunksPerDoc: &c.maxChunksPerDoc,
+			Query:           query,
+			Documents:       c.documentsToStringSlice(documents),
+			TopN:            &c.topN,
+		},
+		resp,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.rerankDocuments(documents, resp.Results), nil
+}
+
+func (c *CohereRerank) documentsToStringSlice(documents []document.Document) []string {
+	strings := make([]string, len(documents))
+	for i, d := range documents {
+		strings[i] = d.Content
+	}
+	return strings
+}
+
+func (c *CohereRerank) rerankDocuments(documents []document.Document, results []model.RerankResult) []document.Document {
+
+	rerankedDocuments := make([]document.Document, 0)
+	for i, result := range results {
+
+		index := result.Index
+		metadata := documents[index].Metadata
+		if metadata == nil {
+			metadata = make(types.Meta)
+		}
+		metadata[CohereRerankScoreMetdataKey] = result.RelevanceScore
+
+		rerankedDocuments = append(
+			rerankedDocuments,
+			document.Document{
+				Content:  documents[index].Content,
+				Metadata: metadata,
+			},
+		)
+
+		if i >= c.topN {
+			break
+		}
+	}
+
+	return rerankedDocuments
+}
