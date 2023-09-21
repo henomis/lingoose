b93ffbb4fbf570be56b18a788fe1b148223602e8
Simone Vellei <henomis@gmail.com>
2023-09-21 09:28:27 +0200

    fix

--- index/simpleVectorIndex/simpleVectorIndex.go
+++ index/simpleVectorIndex/simpleVectorIndex.go
@@ -239,23 +239,23 @@ func (s *Index) similaritySearch(
 }
 
 func (s *Index) cosineSimilarity(a []float64, b []float64) (cosine float64, err error) {
-	count := 0
-	length_a := len(a)
-	length_b := len(b)
-	if length_a > length_b {
-		count = length_a
+	var count int
+	lengthA := len(a)
+	lengthB := len(b)
+	if lengthA > lengthB {
+		count = lengthA
 	} else {
-		count = length_b
+		count = lengthB
 	}
 	sumA := 0.0
 	s1 := 0.0
 	s2 := 0.0
 	for k := 0; k < count; k++ {
-		if k >= length_a {
+		if k >= lengthA {
 			s2 += math.Pow(b[k], 2)
 			continue
 		}
-		if k >= length_b {
+		if k >= lengthB {
 			s1 += math.Pow(a[k], 2)
 			continue
 		}
