e2b50826fa9a2dbd7cc9700dac7f2eb1ce35313c
Simone Vellei <henomis@gmail.com>
2023-12-17 13:04:46 +0100

    fix

--- examples/pipeline/summarize/main.go
+++ examples/pipeline/summarize/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 	summarize := summarizepipeline.New(
-		openai.NewCompletion().WithMaxTokens(1000).WithVerbose(true).WithModel(openai.GPT3TextDavinci002),
+		openai.NewCompletion().WithMaxTokens(1000).WithVerbose(true).WithModel(openai.GPT3Dot5TurboInstruct),
 		loader.NewTextLoader("state_of_the_union.txt", nil).
 			WithTextSplitter(textsplitter.NewRecursiveCharacterTextSplitter(2000, 0)),
 	)
