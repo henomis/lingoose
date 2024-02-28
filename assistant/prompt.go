b45d2da321939247007282515f204c0cfbd991d2
Simone Vellei <henomis@gmail.com>
2024-02-28 08:05:54 +0100

    chore: fix assistant prompt

--- assistant/prompt.go
+++ assistant/prompt.go
@@ -4,11 +4,11 @@ const (
 	//nolint:lll
 	baseRAGPrompt = "Use the following pieces of retrieved context to answer the question.\n\nQuestion: {{.question}}\nContext:\n{{range .results}}{{.}}\n\n{{end}}"
 	//nolint:lll
-	systemRAGPrompt = "You name is {{.assistantName}}, and you are {{.assistantIdentity}} at {{.companyName}} {{if ne .companyDescription \"\" }}{{.companyDescription}}{{end}}. Your task is to assist humans {{.assistantScope}}."
+	systemRAGPrompt = "You name is {{.assistantName}}, and you are {{.assistantIdentity}} {{if ne .companyName \"\" }}at {{.companyName}}{{end}}{{if ne .companyDescription \"\" }}, {{.companyDescription}}{{end}}. Your task is to assist humans {{.assistantScope}}."
 
 	defaultAssistantName      = "AI assistant"
 	defaultAssistantIdentity  = "a helpful and polite assistant"
 	defaultAssistantScope     = "with their questions"
-	defaultCompanyName        = "Lingoose"
+	defaultCompanyName        = ""
 	defaultCompanyDescription = ""
 )
