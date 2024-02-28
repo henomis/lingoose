232a7fbdf558c62cdd3a52ddfdb4766fa901de3d
Simone Vellei <henomis@gmail.com>
2024-02-28 23:24:57 +0100

    fix

--- rag/sub_document.go
+++ rag/sub_document.go
@@ -23,6 +23,7 @@ type SubDocumentRAG struct {
 	llm            LLM
 }
 
+//nolint:lll
 var SubDocumentRAGSummarizePrompt = "Please give a concise summary of the context in 1-2 sentences.\n\nContext: {{.context}}"
 
 func NewSubDocumentRAG(index *index.Index, llm LLM) *SubDocumentRAG {
