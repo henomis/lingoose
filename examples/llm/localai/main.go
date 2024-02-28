f9ccab6ebefc7335a7b15ef3067d6787f71fee5b
Simone Vellei <henomis@gmail.com>
2024-02-28 21:55:28 +0100

    chore: add groq support

---
+++ examples/llm/localai/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/henomis/lingoose/llm/openai"
+	"github.com/henomis/lingoose/thread"
+	goopenai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	customConfig := goopenai.DefaultConfig("")
+	customConfig.BaseURL = "http://localhost:8080"
+	customClient := goopenai.NewClientWithConfig(customConfig)
+
+	openaillm := openai.New().WithClient(customClient).WithModel("ggml-gpt4all-j")
+
+	t := thread.New().AddMessage(
+		thread.NewUserMessage().AddContent(
+			thread.NewTextContent("What's the NATO purpose?"),
+		),
+	)
+
+	err := openaillm.Generate(context.Background(), t)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(t)
+}
