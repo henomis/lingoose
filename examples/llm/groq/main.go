465e21d5d5c5450eb82444ab660ce9b61d5036f7
Simone Vellei <henomis@gmail.com>
2024-02-29 08:40:12 +0100

    chore: add localai and groq llm wrapper (#174)

--- examples/llm/groq/main.go
+++ examples/llm/groq/main.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/henomis/lingoose/llm/openai"
+	"github.com/henomis/lingoose/llm/groq"
 	"github.com/henomis/lingoose/thread"
-	goopenai "github.com/sashabaranov/go-openai"
 )
 
 func main() {
-	customConfig := goopenai.DefaultConfig("YOUR_API_KEY")
-	customConfig.BaseURL = "https://api.groq.com/openai/v1"
-	customClient := goopenai.NewClientWithConfig(customConfig)
-
-	openaillm := openai.New().WithClient(customClient).WithModel("mixtral-8x7b-32768")
+	// The Groq API key is expected to be set in the OPENAI_API_KEY environment variable
+	groqllm := groq.New().WithModel("mixtral-8x7b-32768")
 
 	t := thread.New().AddMessage(
 		thread.NewUserMessage().AddContent(
@@ -22,7 +18,7 @@ func main() {
 		),
 	)
 
-	err := openaillm.Generate(context.Background(), t)
+	err := groqllm.Generate(context.Background(), t)
 	if err != nil {
 		panic(err)
 	}
--- examples/llm/localai/main.go
+++ examples/llm/localai/main.go
@@ -4,17 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/henomis/lingoose/llm/openai"
+	"github.com/henomis/lingoose/llm/localai"
 	"github.com/henomis/lingoose/thread"
-	goopenai "github.com/sashabaranov/go-openai"
 )
 
 func main() {
-	customConfig := goopenai.DefaultConfig("YOUR_API_KEY")
-	customConfig.BaseURL = "http://localhost:8080"
-	customClient := goopenai.NewClientWithConfig(customConfig)
-
-	openaillm := openai.New().WithClient(customClient).WithModel("ggml-gpt4all-j")
+	localaillm := localai.New("http://localhost:8080").WithModel("ggml-gpt4all-j")
 
 	t := thread.New().AddMessage(
 		thread.NewUserMessage().AddContent(
@@ -22,7 +17,7 @@ func main() {
 		),
 	)
 
-	err := openaillm.Generate(context.Background(), t)
+	err := localaillm.Generate(context.Background(), t)
 	if err != nil {
 		panic(err)
 	}
