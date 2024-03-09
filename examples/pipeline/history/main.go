a248fd9e96e490605648ea996b2417f7f40c5003
Simone Vellei <henomis@gmail.com>
2024-03-09 16:40:26 +0100

    chore: move legacy code

--- examples/pipeline/history/main.go
+++ examples/pipeline/history/main.go
@@ -5,10 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/henomis/lingoose/chat"
 	"github.com/henomis/lingoose/history"
+	"github.com/henomis/lingoose/legacy/chat"
+	"github.com/henomis/lingoose/legacy/pipeline"
 	"github.com/henomis/lingoose/llm/openai"
-	"github.com/henomis/lingoose/pipeline"
 	"github.com/henomis/lingoose/prompt"
 	"github.com/henomis/lingoose/types"
 )
--- legacy/pipeline/summarize/summarize.go
+++ legacy/pipeline/summarize/summarize.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/henomis/lingoose/document"
-	"github.com/henomis/lingoose/pipeline"
+	"github.com/henomis/lingoose/legacy/pipeline"
 	"github.com/henomis/lingoose/prompt"
 	"github.com/henomis/lingoose/types"
 )
