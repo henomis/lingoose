9b7f6a1c354b623ab6583d81781334046b81bee4
Simone Vellei <henomis@gmail.com>
2024-03-09 17:29:52 +0100

    feat: add voyage embedd and rerank

---
+++ examples/embeddings/voyage/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	voyageembedder "github.com/henomis/lingoose/embedder/voyage"
+	"github.com/henomis/lingoose/index"
+	indexoption "github.com/henomis/lingoose/index/option"
+	"github.com/henomis/lingoose/index/vectordb/jsondb"
+	"github.com/henomis/lingoose/llm/antropic"
+	"github.com/henomis/lingoose/loader"
+	"github.com/henomis/lingoose/textsplitter"
+	"github.com/henomis/lingoose/thread"
+	"github.com/henomis/lingoose/types"
+)
+
+// download https://raw.githubusercontent.com/hwchase17/chat-your-data/master/state_of_the_union.txt
+
+func main() {
+
+	index := index.New(
+		jsondb.New().WithPersist("db.json"),
+		voyageembedder.New().WithModel("voyage-2"),
+	).WithIncludeContents(true).WithAddDataCallback(func(data *index.Data) error {
+		data.Metadata["contentLen"] = len(data.Metadata["content"].(string))
+		return nil
+	})
+
+	indexIsEmpty, _ := index.IsEmpty(context.Background())
+
+	if indexIsEmpty {
+		err := ingestData(index)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	query := "What is the purpose of the NATO Alliance?"
+	similarities, err := index.Query(
+		context.Background(),
+		query,
+		indexoption.WithTopK(3),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, similarity := range similarities {
+		fmt.Printf("Similarity: %f\n", similarity.Score)
+		fmt.Printf("Document: %s\n", similarity.Content())
+		fmt.Println("Metadata: ", similarity.Metadata)
+		fmt.Println("----------")
+	}
+
+	documentContext := ""
+	for _, similarity := range similarities {
+		documentContext += similarity.Content() + "\n\n"
+	}
+
+	antropicllm := antropic.New().WithModel("claude-3-opus-20240229")
+	t := thread.New()
+	t.AddMessage(thread.NewUserMessage().AddContent(
+		thread.NewTextContent("Based on the following context answer to the" +
+			"question.\n\nContext:\n{{.context}}\n\nQuestion: {{.query}}").Format(
+			types.M{
+				"query":   query,
+				"context": documentContext,
+			},
+		),
+	))
+
+	err = antropicllm.Generate(context.Background(), t)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(t)
+}
+
+func ingestData(index *index.Index) error {
+
+	fmt.Printf("Ingesting data...")
+
+	documents, err := loader.NewDirectoryLoader(".", ".txt").Load(context.Background())
+	if err != nil {
+		return err
+	}
+
+	textSplitter := textsplitter.NewRecursiveCharacterTextSplitter(1000, 20)
+
+	documentChunks := textSplitter.SplitDocuments(documents)
+
+	err = index.LoadFromDocuments(context.Background(), documentChunks)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Done!\n")
+
+	return nil
+}
---
+++ examples/transformer/voyage-rerank/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/henomis/lingoose/document"
+	"github.com/henomis/lingoose/transformer"
+)
+
+func main() {
+
+	r := transformer.NewVoyageRerank()
+
+	documents, err := r.Rerank(
+		context.Background(),
+		"What is the capital of the United States?",
+		[]document.Document{
+			{
+				Content: "Carson City is the capital city of the American state of Nevada.",
+			}, {
+				Content: "Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district.",
+			}, {
+				Content: "Capital punishment (the death penalty) has existed in the United States since beforethe United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states.",
+			},
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, doc := range documents {
+		fmt.Println(doc.GetEnrichedContent())
+		fmt.Println("-----")
+	}
+}
